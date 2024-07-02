f3a6b16f0bcfcda6dc465a599c81bb5f5b818d04
alif <aliftareq@gmail.com>
2024-07-02 12:37:00 +0600

    room  module CRUD operation completed

--- src/app/modules/room/room.route.ts
+++ src/app/modules/room/room.route.ts
@@ -5,10 +5,18 @@ import { RoomValidation } from './room.validation';
 
 const router = express.Router();
 
+router.get('/', RoomController.getAllFromDB);
+router.get('/:id', RoomController.getByIdFromDB);
 router.post(
   '/',
   validateRequest(RoomValidation.create),
   RoomController.insertIntoDB
 );
+router.patch(
+  '/:id',
+  validateRequest(RoomValidation.update),
+  RoomController.updateOneInDB
+);
+router.delete('/:id', RoomController.deleteByIdFromDB);
 
 export const RoomRoutes = router;
