f5e30dd745818d46c5da389f1c8635e2464527af
alif <aliftareq@gmail.com>
2024-07-07 10:00:38 +0600

    confirm my reg api creation completed

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -106,6 +106,21 @@ const withdrawFromCourse = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const confirmMyRegistration = catchAsync(
+  async (req: Request, res: Response) => {
+    const user = (req as any).user;
+    const result = await SemesterRegistrationService.confirmMyRegistration(
+      user?.userId
+    );
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Confirm your registration!',
+      data: result,
+    });
+  }
+);
+
 export const SemesterRegistrationController = {
   insertIntoDB,
   getAllFromDB,
@@ -115,4 +130,5 @@ export const SemesterRegistrationController = {
   startMyRegistration,
   enrollIntoCourse,
   withdrawFromCourse,
+  confirmMyRegistration,
 };
--- src/app/modules/semesterRegistration/semesterRegistration.routes.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.routes.ts
@@ -41,4 +41,9 @@ router.post(
   SemesterRegistrationController.withdrawFromCourse
 );
 
+router.post(
+  '/confirm-my-registration',
+  SemesterRegistrationController.confirmMyRegistration
+);
+
 export const semesterRegistrationRoutes = router;
